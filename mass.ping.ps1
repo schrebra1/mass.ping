a348dda612dd96d89a17ee589ea583eedb86d3dd
schrebra1 <98029607+schrebra1@users.noreply.github.com>
2022-08-30 08:26:29 -0700

    Add files via upload

---
+++ mass.ping.ps1
@@ -0,0 +1,39 @@
+﻿$computers = get-content \\example.share\test.computers.txt 
+
+
+
+
+
+
+
+  workflow test
+{
+  param($computers)
+
+    foreach -parallel ($computer in $computers)
+    {
+        $status = Test-Connection -ComputerName $computer -Count 1 -Quiet
+
+        if (!$status)
+            {
+            
+            Write-Output "Offline"
+            
+            }
+
+            else
+            { 
+            
+            Write-host "Online"
+            
+            }
+           
+    }
+
+    $computers = get-content \\example.share\test.computers.txt 
+    
+}
+
+
+
+test $computers
